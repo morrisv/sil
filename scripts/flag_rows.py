4e2635f11ed2e078eada30b5575939ec21518d1d
mcflugen <mcflugen@gmail.com>
2016-04-15 15:15:34 -0600

    Added ability to read ranges from a file.

--- scripts/flag_rows.py
+++ scripts/flag_rows.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 from __future__ import print_function
 
+import os
 import sys
 
 import numpy as np
@@ -16,14 +17,16 @@ def main():
     parser.add_argument('--data', type=argparse.FileType('r'),
                         default=sys.stdin, help='Text data file')
     parser.add_argument('rows', nargs='*', help='Rows to flag')
+    parser.add_argument('--file', type=argparse.FileType('r'), default=[],
+                        help='File containing ranges')
     parser.add_argument('--flag', help='Flag to add')
 
     args = parser.parse_args()
 
     data = pandas.read_table(args.data)
 
-    if args.rows:
-        rows = expand_ranges(args.rows)
+    rows = expand_ranges(args.rows) + expand_ranges(args.file)
+    if rows:
         data.loc[rows, 'flags'] = args.flag
     else:
         data.loc[:, 'flags'] = args.flag
